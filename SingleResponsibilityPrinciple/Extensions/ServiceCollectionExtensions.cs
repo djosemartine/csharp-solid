8bfd38f81314f012b6611936b27cc8c0403a9375
Daniel <Daniel.Martinez@seon.com>
2019-08-18 13:29:41 -0500

    Extensión Method for configuration services added

---
+++ SingleResponsibilityPrinciple/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SingleResponsibilityPrinciple.Services;
+
+namespace SingleResponsibilityPrinciple.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static ServiceProvider ConfigureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileSaverService, FileSaverService>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SingleResponsibilityPrinciple.Extensions;
 using SingleResponsibilityPrinciple.Models;
 using SingleResponsibilityPrinciple.Services;
 using System;
@@ -12,21 +13,15 @@ namespace SingleResponsibilityPrinciple
             Console.WriteLine("Single Responsibilty Principle");
             // Register services
             var services = new ServiceCollection();
-            var serviceProvider = ConfigureServices(services);
+            var serviceProvider = services.ConfigureServices();
             // Get Services
             var fileSaverService = serviceProvider.GetService<IFileSaverService>();
 
             var report = new WorkReport();
             report.AddEntry(new WorkReportEntry { Name = "Entry 1", ProjectCode = "CO-100", SpentHours = 10 });
             report.AddEntry(new WorkReportEntry { Name = "Entry 2", ProjectCode = "CO-101", SpentHours = 20 });
-            fileSaverService.SaveToFile("Reports", "SRP-Report.txt", report);
+            fileSaverService.SaveToFile(@"D:\\Reports", "SRP-Report.txt", report);
             Console.WriteLine(report.ToString());
-        }
-
-        private static ServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<IFileSaverService, FileSaverService>();
-            return services.BuildServiceProvider();
         }
     }
 }
